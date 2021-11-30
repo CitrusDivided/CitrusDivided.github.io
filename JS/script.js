132643056daa96beb7ea13ea208a302ba1614d06
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2021-11-30 13:29:46 -0800

    MAIN

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
-empty
+var canvas = document.querySelector('canvas')
+;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.fillStyle = "#2AFF94";
+c.fillRect(100, 115, 100, 100);
+c.fillRect(750, 210, 155, 195);
+c.fillRect(245, 255, 100, 100);
+
+console.log(canvas);
+
+// Line
+c.beginPath();
+c.strokeStyle = '#2AFF94'
+c.moveTo(150,165);
+c.lineTo(300,165);
+c.lineTo(300,300);
+c.lineTo(750, 300);
+c.stroke();
+
+// Text
+c.fillStyle = "#2AFF94";
+c.font = '28px serif';
+c.textAlign = 'center';
+c.fillText('Testing',820,155);empty
