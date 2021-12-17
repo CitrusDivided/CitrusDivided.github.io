1cb71ac8ca1d907e570f84f17a0330f983c38a0b
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2021-12-17 15:35:14 -0800

    JS: SnowFall Positional Array Change

--- JS/script.js
+++ JS/script.js
@@ -57,7 +57,7 @@ for (var i = 0; i < 300; i++){
     var t = 200;
     var tx = 1
     //Array
-    var ssi_x = Math.random() * 200;
+    var ssi_x = Math.random() * 8;
     var ssi_y = Math.random() * 400;
     var ssi_dx = (Math.random() -0.5) *3;
     var ssi_dy = (Math.random() -0.5)  *3;
