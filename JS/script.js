a747c095b339ade430add4e0f41a56855a15e2ac
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2021-12-16 18:03:14 -0800

    JS: Brought back into testing on Snow Array Track

--- JS/script.js
+++ JS/script.js
@@ -71,7 +71,7 @@ for (var i = 0; i < 16; i++){
     var box = c.createLinearGradient(5,180,0,480,);
     //var falling = c.arc(falling,falling,50,0, Math.PI * 2, false);//
     var gradient = c.createRadialGradient(x,y,3, x,y,7);
-    snowArray.push(new Snow(ssi_x,tx,ssi_radius,));
+    snowArray.push(new Snow(ssi_x,ssi_dy,ssi_radius,));
     //var snow = new Snow(ssi_x,ssi_y,ssi_radius,3,300);
     
     
