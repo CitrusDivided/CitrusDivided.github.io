569d8325e17dc383d4ea9080d2bc8dfc48e55eda
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2022-01-22 01:09:32 -0800

    JS: Adding sections based on studies

--- JS/projectscript2.js
+++ JS/projectscript2.js
@@ -13,3 +13,13 @@ c.fillStyle = "rgb(255,55,55,0.2)";
 c.font = '18px serif';
 c.textAlign = 'left';
 c.fillText('Hematological models snippet',25,35);
+
+// Heme
+c.arc(754,297, 15,0, Math.PI * 2, false)
+c.strokeStyle = "rgb(242,255,148,0.3"
+c.fillStyle = "rgb(255,200,255,0.9";
+c.stroke();
+c.fill();
+
+// Beta (β) polypeptide chains
+// Alpha (α) polypeptide chains
