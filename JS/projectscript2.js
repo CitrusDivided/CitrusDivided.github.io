8edd0be858cce0859d0dcd1b6065d24074c345cb
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2022-01-22 01:04:35 -0800

    JS: Made changes to text render, placeholder.

--- JS/projectscript2.js
+++ JS/projectscript2.js
@@ -7,3 +7,9 @@ canvas.width = 800;
 canvas.height = 480;
 
 var c = canvas.getContext('2d');
+
+// Text
+c.fillStyle = "rgb(255,55,55,0.2)";
+c.font = '18px serif';
+c.textAlign = 'left';
+c.fillText('Hematological models snippet',25,35);
