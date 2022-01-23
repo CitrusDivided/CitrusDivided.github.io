4bad5c853a235f20ebaa6903d953ec5afc31b576
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2022-01-23 12:01:09 -0800

    JS: Coloring for Hb

--- JS/projectscript2.js
+++ JS/projectscript2.js
@@ -14,10 +14,10 @@ c.font = '18px serif';
 c.textAlign = 'left';
 c.fillText('Hematological models snippet',25,35);
 
-// Heme
-c.arc(754,297, 15,0, Math.PI * 2, false)
-c.strokeStyle = "rgb(242,255,148,0.3"
-c.fillStyle = "rgb(255,200,255,0.9";
+// Heme (Hb)
+c.arc(200,300, 55,0, Math.PI * 2, false)
+c.strokeStyle = "rgb(255,1,148,1";
+c.fillStyle = "rgb(255,100,100,0.9";
 c.stroke();
 c.fill();
 
