1f115218f18191bcc5c8523585dd856be1c234b5
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2022-01-23 12:26:18 -0800

    JS: Imp. Gradient Hb

--- JS/projectscript2.js
+++ JS/projectscript2.js
@@ -8,6 +8,9 @@ canvas.height = 480;
 
 var c = canvas.getContext('2d');
 
+// Hb Gradient Set
+var gradient = c.createRadialGradient(400,400,55, 100,100,50);
+
 // Text
 c.fillStyle = "rgb(255,55,55,0.2)";
 c.font = '18px serif';
@@ -15,14 +18,31 @@ c.textAlign = 'left';
 c.fillText('Hematological models snippet',25,35);
 
 // Heme (Hb)
+        // Gradient Subset
+        gradient.addColorStop(.3, "rgb(255,255,255,0.9");
+        gradient.addColorStop(.3, "rgba(255,0,0,0.9");
+;
+c.beginPath();
+c.strokeStyle = "rgb(255,0,0,1";
+c.fillStyle = gradient;
 c.arc(200,300, 55,0, Math.PI * 2, false)
+c.fillStyle = gradient;
 c.arc(320,300, 55,0, Math.PI * 2, false)
-c.arc(320,180, 55,0, Math.PI * 2, false)
-c.arc(200,180, 55,0, Math.PI * 2, false)
-c.strokeStyle = "rgb(255,1,148,1";
-c.fillStyle = "rgb(255,100,100,0.9";
+
 c.stroke();
 c.fill();
+c.beginPath();
+c.arc(320,180, 55,0, Math.PI * 2, false)
+c.arc(200,180, 55,0, Math.PI * 2, false)
+c.stroke();
+c.fill();
+
+// Hb Descriptor
+c.beginPath();
+c.strokeStyle = "rgb(255,0,0,1";
+c.moveTo(500,105);
+c.lineTo(350,135);
+c.stroke();
 
 // Beta (β) polypeptide chains
 // Alpha (α) polypeptide chains
