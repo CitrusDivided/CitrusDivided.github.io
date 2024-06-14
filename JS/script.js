0d8ebbb058b233053f3e1a16654a6e5184b8885d
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2024-06-13 17:18:08 -0700

    Creating base preview of M0, further updating information.

--- JS/script.js
+++ JS/script.js
@@ -34,8 +34,8 @@ var scannerColoring = anime.timeline({
 
 var canvas = document.querySelector('canvas');
 
-canvas.width = 800;
-canvas.height = 480;
+canvas.width = 600;
+canvas.height = 960;
 
 var c = canvas.getContext('2d');
 // Interaction //
