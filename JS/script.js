69765b8eee554d4d7dc2abfb1f7881fa41b3b2a2
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2024-08-11 14:48:24 -0700

    Properly restructured all of page-headline-base into grid system, removing margins;

--- JS/script.js
+++ JS/script.js
@@ -28,7 +28,7 @@ anime({
 anime({
     targets: ['#pageHeadlineHoloSheen'],
     duration: 9200,
-    top: '38%',
+    top: '30%',
     loop: false,
     direction: 'alternate',
     easing: 'easeInOutExpo'
